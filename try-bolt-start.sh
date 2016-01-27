ca0206978ae693c2c6f0947e236829fa7b6ca0be
Ross Riley <riley.ross@gmail.com>
2016-01-27 01:05:06 +0000

    fix start script

--- try-bolt-start.sh
+++ try-bolt-start.sh
@@ -1,4 +1,4 @@
-ln -sf `pwd`/config/#{fetch(:stage)}.php `pwd`/config/config.ph
+ln -sf `pwd`/config/production.php `pwd`/config/config.php
 ./console migrations:migrate --no-interaction
 ./console orm:generate-proxies
 ./console bolt:demo-runner
